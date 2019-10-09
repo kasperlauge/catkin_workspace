3d191386145030b8c73d31c931e44d6ed64cc71d
larsH <larshjerrild@hotmail.com>
2019-10-09 07:49:12 +0200

    minor cleanup

--- src/drone_controller/src/uav.cpp
+++ src/drone_controller/src/uav.cpp
@@ -1,19 +1,9 @@
 #include "uav.h"
 
-void UAV::rotate(float rad)
-{
-    tf2::Quaternion q_orig, q_rot, q_new;
-
-    tf2::convert(this->pose.pose.orientation, q_orig);
-
-    q_rot.setRPY(0, 0, rad);
-
-    q_new = q_rot * q_orig;
-    q_new.normalize();
-
-    tf2::convert(q_new, this->pose.pose.orientation);
-}
-
+/**
+ * Function to publish position preflight, needed to make offboard turn on
+ * 
+ */
 void UAV::setInitialPosition()
 {
     tf2::Quaternion q_orig;
@@ -29,22 +19,28 @@ void UAV::setInitialPosition()
     setPose(tmp);
 }
 
-void UAV::run()
+void UAV::setup()
 {
-
-    ROS_INFO("RUN Called");
+    auto stateTopic = "/uav0/mavros/state";
+    this->stateSubscriber = this->nodeHandle.subscribe<mavros_msgs::State>(stateTopic, 1, &UAV::state_cb, this);
     this->local_pos_pub = this->nodeHandle.advertise<geometry_msgs::PoseStamped>("/uav0/mavros/setpoint_position/local", 10);
     this->arming_client = this->nodeHandle.serviceClient<mavros_msgs::CommandBool>("/uav0/mavros/cmd/arming");
     this->set_mode_client = this->nodeHandle.serviceClient<mavros_msgs::SetMode>("/uav0/mavros/set_mode");
     this->get_image_client = this->nodeHandle.serviceClient<image_sampling::GetSampledImages>("/GetImage");
     this->slz_recognition_client = this->nodeHandle.serviceClient<slz_recognition::ImageInfo>("find_slz");
+}
+
+void UAV::run()
+{
+
+    ROS_INFO("RUN Called");
 
     std::vector<sensor_msgs::Image> sample_images;
 
     // Cannot exceed 500ms due to The px4 flight stack has a timeout of 500ms between two Offboard commands
     // https://dev.px4.io/v1.9.0/en/ros/mavros_offboard.html
     ros::Rate rate(20.0);
-    
+
     // Wait for connection
     while (ros::ok() && !current_state.connected)
     {
@@ -105,22 +101,6 @@ void UAV::run()
         rate.sleep();
         counter++;
     }
-
-    slz_recognition::ImageInfo slz_msg;
-    slz_msg.request.Images = sample_images;
-
-    if (slz_recognition_client.call(slz_msg))
-    {
-
-        std::tuple<int, int> p1 = std::make_pair(slz_msg.response.x[0], slz_msg.response.y[0]);
-        slz_positions.push_back(p1);
-
-        ROS_INFO("places are : %d", slz_msg.response.x[0]);
-    }
-    else
-    {
-        ROS_WARN("Could not reach slz_recognition service");
-    }
 }
 
 void UAV::setPose(geometry_msgs::PoseStamped _pose)
@@ -133,15 +113,19 @@ UAV::UAV(ros::NodeHandle n)
     this->nodeHandle = n;
 
     // Set Callback for being able to monitor State
-    auto stateTopic = "/uav0/mavros/state";
-    this->stateSubscriber = this->nodeHandle.subscribe<mavros_msgs::State>(stateTopic, 1, &UAV::state_cb, this);
+    
     this->_flightstrat = new SampleStrat(this->nodeHandle);
+    this->setup();
 };
 
+/**
+ * Will change when there is a set strategy function.
+ */
 UAV::~UAV()
 {
     delete _flightstrat;
 }
+
 void UAV::state_cb(const mavros_msgs::State::ConstPtr &msg)
 {
     this->current_state = *msg;
