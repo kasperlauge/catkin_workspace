562597754d693c97c1afe9e3a6b17ace9922464f
larsh <larshjerrild@hotmail.com>
2019-10-16 14:02:24 +0200

    Coordinate transform now outputs floats as expected

--- src/coordinate_transformation/src/transformer.cpp
+++ src/coordinate_transformation/src/transformer.cpp
@@ -1,4 +1,8 @@
 #include "transformer.h"
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/filter.h>
 
 Transformer::Transformer(ros::NodeHandle n)
 {
@@ -11,50 +15,44 @@ bool Transformer::transformCoordinates(coordinate_transformation::CoordinateInfo
 
     std::vector<sensor_msgs::PointCloud2> pointClouds(req.SlzData.PointClouds);
 
-    std::vector<slz_recognition::ImageCoordinateData> imageInfos = req.SlzData.ImageCoordinateData;
+    std::vector<slz_recognition::ImageCoordinateData> imageInfos(req.SlzData.ImageCoordinateData);
     auto numberOfPointClouds = imageInfos.size();
     res.CoordinateData.reserve(numberOfPointClouds);
 
     ROS_INFO("allocated vectors");
 
-    ROS_INFO_STREAM("imagesize: " << imageInfos.size());
-
+    ROS_INFO_STREAM("nr. of images: " << imageInfos.size());
 
     // Loop through all of the sampled image data - for each imagedata
-    for(std::vector<int>::size_type i = 0; i != imageInfos.size(); i++) {
-        // std::cout << static_cast<unsigned>(it->x.at(0)) << std::endl;
+    for (std::vector<int>::size_type i = 0; i != imageInfos.size(); i++)
+    {
+        // Convert cloud to PCL::XYZ
+        pcl::PCLPointCloud2 pcl_pc2;
+        pcl_conversions::toPCL(pointClouds[i], pcl_pc2);
+        pcl::PointCloud<pcl::PointXYZ>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+        pcl::fromPCLPointCloud2(pcl_pc2, *temp_cloud);
+
         coordinate_transformation::CoordinateData coordinateData;
         coordinateData.x.reserve(imageInfos.at(i).x.size());
         coordinateData.y.reserve(imageInfos.at(i).x.size());
         coordinateData.z.reserve(imageInfos.at(i).x.size());
-        ROS_INFO_STREAM("outer loop: " << i);
 
-        for (int j = 0; j < imageInfos.at(i).x.size(); j++) {
-            ROS_INFO_STREAM("inner loop: " << j);
-            // https://answers.ros.org/question/191265/pointcloud2-access-data/
-            auto pointCloud = pointClouds.at(i);
-            int u = imageInfos.at(i).x.at(j);
-            int v = imageInfos.at(i).y.at(j);
-            int arrayPosition = v*pointCloud.row_step + u*pointCloud.point_step;
+        ROS_INFO("%d", imageInfos.at(i).x.size());
 
-            // compute position in array where x,y,z data start
-            int arrayPosX = arrayPosition + pointCloud.fields[0].offset; // X has an offset of 0
-            int arrayPosY = arrayPosition + pointCloud.fields[1].offset; // Y has an offset of 4
-            int arrayPosZ = arrayPosition + pointCloud.fields[2].offset; // Z has an offset of 8
+        for (size_t j = 0; j < imageInfos.at(i).x.size(); j++)
+        {
+            const u_int32_t index = imageInfos.at(i).x.at(j) + 800 * imageInfos.at(i).y.at(j);
 
-            int x = pointCloud.data[arrayPosX];
-            int y = pointCloud.data[arrayPosY];
-            int z = pointCloud.data[arrayPosZ];
-
-            coordinateData.x.push_back(x);
-            coordinateData.y.push_back(y);
-            coordinateData.z.push_back(z);
+            if (!isnan(temp_cloud->points[index].x))
+            {
+                coordinateData.x.push_back(temp_cloud->points[index].x);
+                coordinateData.y.push_back(temp_cloud->points[index].y);
+                coordinateData.z.push_back(temp_cloud->points[index].z);
+            }
         }
 
         res.CoordinateData.push_back(coordinateData);
     }
-
-    ROS_INFO("Done looping");
 
     return true;
 };
