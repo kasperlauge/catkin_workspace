067340b0b6b3df9e8b9de97d99044f6cf4401f04
larsh <larshjerrild@hotmail.com>
2019-10-16 11:04:49 +0200

    Added visualization in RVIZ.

---
+++ src/tf_node/scripts/main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import tf
+
+import tf2_ros
+import geometry_msgs.msg
+import rospy
+import math
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('my_static_tf2_broadcaster')
+        broadcaster = tf2_ros.StaticTransformBroadcaster()
+        static_transformStamped = geometry_msgs.msg.TransformStamped()
+        
+        static_transformStamped.header.frame_id = "base_link"
+        static_transformStamped.child_frame_id = "iris_sensors_0/camera_red_iris_link"
+    
+
+        rospy.loginfo("message")
+        static_transformStamped.transform.translation.x = float(0)
+        static_transformStamped.transform.translation.y = float(0)
+        static_transformStamped.transform.translation.z = float(0)
+
+        quat = tf.transformations.quaternion_from_euler(
+                   float(-math.pi*0.5),float(0),float(-math.pi*0.5))
+        static_transformStamped.transform.rotation.x = quat[0]
+        static_transformStamped.transform.rotation.y = quat[1]
+        static_transformStamped.transform.rotation.z = quat[2]
+        static_transformStamped.transform.rotation.w = quat[3]
+        broadcaster.sendTransform(static_transformStamped)
+        rospy.spin()
+
+    except rospy.ROSInterruptException:
+        pass
