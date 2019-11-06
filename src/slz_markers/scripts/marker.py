9397636b74a4dc4b08bd929147900327bf4a0516
larsH <larshjerrild@hotmail.com>
2019-11-06 07:17:42 +0100

    Added clustering

--- src/slz_markers/scripts/marker.py
+++ src/slz_markers/scripts/marker.py
@@ -19,44 +19,50 @@ def mycallback(msg):
         data[1][i] = pos.y[i]
         data[2][i] = pos.z[i]       
 
-    kmeans = KMeans(n_clusters=2, random_state=0).fit(data)
+    rospy.loginfo(data.shape)
     
-    cluster1 = np.where(kmeans.labels_ == 0)
-    cluster2 = np.where(kmeans.labels_ == 1)
+    nr_clusters = 7
 
-    for i in data[cluster1]:
-        marker = Marker()
-        marker.header.frame_id = "iris_sensors_0/camera_red_iris_link"
-        marker.type = marker.SPHERE
-        marker.action = marker.ADD
-        marker.scale.x = 0.2
-        marker.scale.y = 0.2
-        marker.scale.z = 0.2
-        marker.color.a = 1.0
-        marker.color.r = 1.0
-        marker.color.g = 1.0
-        marker.color.b = 0.0
-        marker.pose.position.x = i[0]
-        marker.pose.position.y = i[1]
-        marker.pose.position.z = i[2]
-        markerArray.markers.append(marker)
+    kmeans = KMeans(n_clusters=nr_clusters, random_state=0).fit(np.transpose(data))
     
-    for i in data[cluster2]:
-        marker = Marker()
-        marker.header.frame_id = "iris_sensors_0/camera_red_iris_link"
-        marker.type = marker.SPHERE
-        marker.action = marker.ADD
-        marker.scale.x = 0.4
-        marker.scale.y = 0.4
-        marker.scale.z = 0.4
-        marker.color.a = 1.0
-        marker.color.r = 1.0
-        marker.color.g = 0.0
-        marker.color.b = 0.0
-        marker.pose.position.x = i[0]
-        marker.pose.position.y = i[1]
-        marker.pose.position.z = i[2]
-        markerArray.markers.append(marker)
+    clusters = []
+    for i in range(nr_clusters):
+        clusters.append(np.where(kmeans.labels_ == i)[0])
+
+    color = [0.1, 0.2,0.3,0.4,0.5,0.6,0.7,0.8]
+    l = 0
+
+    threshold = .1
+
+    remove = []
+
+    for c in clusters:
+        a = np.max(data[2][c[:]])-np.min(data[2][c[:]])
+        rospy.loginfo(a)
+        if a > threshold:
+            remove.append(c)
+
+    for c in remove:
+        clusters.remove(c)
+
+    for c in clusters:
+        for i in range(c.size):
+            marker = Marker()
+            marker.header.frame_id = "iris_sensors_0/camera_red_iris_link"
+            marker.type = marker.SPHERE
+            marker.action = marker.ADD
+            marker.scale.x = 0.2
+            marker.scale.y = 0.2
+            marker.scale.z = 0.2
+            marker.color.a = 1.0
+            marker.color.r = color[l]
+            marker.color.g = color[l]
+            marker.color.b = color[l]
+            marker.pose.position.x = data[0][c[i]]
+            marker.pose.position.y = data[1][c[i]]
+            marker.pose.position.z = data[2][c[i]]
+            markerArray.markers.append(marker)
+        l += 1
     
     id = 0
     for m in markerArray.markers:
@@ -77,26 +83,6 @@ if __name__ == '__main__':
         topic_listener = rospy.Subscriber("slz_coordinates",SLZCoordinates,mycallback)
 
         rospy.spin()
-
-        # while not rospy.is_shutdown():
-        #     marker = Marker()
-        #     marker.header.frame_id = "/base_link"
-        #     marker.type = marker.SPHERE
-        #     marker.action = marker.ADD
-        #     marker.scale.x = 0.2
-        #     marker.scale.y = 0.2
-        #     marker.scale.z = 0.2
-        #     marker.color.a = 1.0
-        #     marker.color.r = 1.0
-        #     marker.color.g = 1.0
-        #     marker.color.b = 0.0
-        #     marker.pose.position.x = 1.
-        #     marker.pose.position.y = 1. 
-        #     marker.pose.position.z = 4. 
-
-        #     pub.publish(marker)
-
-        #     rate.sleep()
 
     except rospy.ROSInterruptException:
         pass
